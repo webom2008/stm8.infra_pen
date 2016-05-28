c48f9739f2f802faa2c36926a68dac8303ab68ce
webom2008 <webom2008@hotmail.com>
2016-05-28 13:32:48 +0800

    [feature]添加红外模块

---
+++ STM8_Demo/Userdef/inc/infrared.h
@@ -0,0 +1,38 @@
+/******************************************************************************
+
+   Copyright (C), 2005-2015, CVTE.
+
+ ******************************************************************************
+  File Name     : infrared.h
+  Version       : Initial Draft
+  Author        : qiuweibo
+  Created       : 2016/5/28
+  Last Modified :
+  Description   : infrared.c header file
+  Function List :
+  History       :
+  1.Date        : 2016/5/28
+    Author      : qiuweibo
+    Modification: Created file
+
+******************************************************************************/
+
+#ifndef __INFRARED_H__
+#define __INFRARED_H__
+
+
+#ifdef __cplusplus
+#if __cplusplus
+extern "C"{
+#endif
+#endif /* __cplusplus */
+
+
+#ifdef __cplusplus
+#if __cplusplus
+}
+#endif
+#endif /* __cplusplus */
+
+
+#endif /* __INFRARED_H__ */
